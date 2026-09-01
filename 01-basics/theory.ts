9fe38b0a72e2ac05b0f375c5c6d0b8202c9a6a6f
xycu <smonindemid@gmail.com>
2026-09-01 17:09:48 +0500

    feat: Add how to specify lightweight types in TypeScript

---
+++ 01-basics/theory.ts
@@ -0,0 +1,7 @@
+const age: number = 20; // numbers -12 or 12 or 23.43
+const userName: string = 'Demid'; // strings "" or ''
+const work: boolean = true; // true or false
+const coding: undefined = undefined; // undefined используем с undefined
+const github: null = null; // null используем с null
+
+const cooking: any = 1; // any любой тип, лучше не использовать

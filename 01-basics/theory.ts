a0f81cbd526bd511583d50086f3b288ec87dcea9
xycu <smonindemid@gmail.com>
2026-09-03 19:04:56 +0500

    feat: add usage examples

--- 01-basics/theory.ts
+++ 01-basics/theory.ts
@@ -62,7 +62,7 @@ function o18(status: 'ok' | 'error') {
 
 // 1 вариант
 // @ts-ignore
-const o18result: boolean = o18('ok'); // не можем положить в переменную boolean null "@ts-ignore" исправит эту проблему
+const o18result: boolean = o18('ok'); // не можем положить в переменную "boolean" "null". "@ts-ignore" исправит эту проблему
 
 // 2 вариант
 // использовать "any" , лучше не использовать!
@@ -74,3 +74,11 @@ const o18result2: any = o18('ok');
 // 4 вариант 
 // Костование типов через "as"
 const o18result4 = o18('ok') as unknown as boolean;
+
+// Как использовать (лучше не злоупотреблять этим)
+const one = [] as unknown as boolean;
+const two = {} as unknown as boolean;
+(two as unknown as {name: 'Demid'}).name;
+(one as unknown as [0,1,2,3,4,5,6])[0];
+
+// as unknown as нужен для того, чтобы привести какой-то существующий тип к новому

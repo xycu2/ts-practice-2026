abf79668af7154f7a9515339d8904631840f950a
xycu <smonindemid@gmail.com>
2026-09-05 23:03:46 +0500

    refactor: Move primitive types to a separate ts file

---
+++ 01-basics/01-basic-types.ts
@@ -0,0 +1,39 @@
+// Примитивы
+const age: number = 20; // numbers -12 or 12 or 23.43
+const userName: string = 'Demid'; // strings "" or ''
+const work: boolean = true; // true or false
+const coding: undefined = undefined; // undefined используем с undefined
+const github: null = null; // null используем с null
+
+const cooking: any = 1; // any любой тип, лучше не использовать
+
+// functions
+function sayHello(a: string, b: number, c: boolean):string {
+  return 'hello'
+};
+
+const sayWork = (a: string, b: number, c: boolean): string => {
+  return 'working'
+};
+
+// Object
+
+// "?" говорит ts, что это поле не обязательно , оно может быть, а может и нет
+type User = {name: string; age: number; car?: {color : string}};
+// type и interface выполняет одно и тоже
+interface User2 {name: string; age: number; car: {color : string}};
+
+const userInfo: User = {
+  name: 'alex',
+  age: 22,
+  car: {
+    color: 'red'
+  }
+};
+
+// Arrays
+const arr1: string[] = ['123', '1234434', '234234'];
+const arr2: Array<string> = ['123', '1234434', '234234'];
+
+
+export {}

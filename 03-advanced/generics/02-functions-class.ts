24d5c28d3e662edb50314d21df4e87561eb460bb
xycu <smonindemid@gmail.com>
2026-09-12 17:51:32 +0500

    feat: add the use of generics to a regular function

---
+++ 03-advanced/generics/02-functions-class.ts
@@ -0,0 +1,15 @@
+// дженерики 
+
+// обычная функция
+function wrapInArray<T>(value: T): T[] {
+  return Array.isArray(value) ? value : [value];
+};
+
+// TS сам понимает, что T = number (явное указание <number> не требуется)
+// const singleNumber = wrapInArray<number>(1);
+const singleNumber = wrapInArray(1);
+
+const singleNumber1 = wrapInArray('dfgdfgd');
+const arrayNumbers = wrapInArray([1,2,3]);
+
+

2dbc5dca7ff4614bc64183788cbe4e1b4d5ebe0e
xycu <smonindemid@gmail.com>
2026-09-13 11:51:23 +0500

    feat: add an example using extends

--- 03-advanced/generics/03-extends.ts
+++ 03-advanced/generics/03-extends.ts
@@ -12,4 +12,15 @@ wrapInArray('hello');
 // Ошибка: мы не можем вызывать с типом данных boolean
 // wrapInArray(true);
 
+// example 2
+// T обязан иметь свойство length типа number (массивы, строки)
+function getLength<T extends { length: number }>(item: T): number {
+  return item.length
+}
+getLength('hello')
+getLength([1,2,3])
+
+// getLength(123) // error: у числа нет св-ва length!
+
+
 export {}

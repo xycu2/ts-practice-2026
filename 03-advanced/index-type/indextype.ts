8c0160a2738f674465a2acf8b9e4ef8f48d4c7e8
xycu <smonindemid@gmail.com>
2026-09-21 19:20:32 +0500

    feat: add an index type

---
+++ 03-advanced/index-type/indextype.ts
@@ -0,0 +1,9 @@
+type User = {
+  name: string,
+  age: number
+}
+
+// 1. keyof User      -> получает Union всех ключей: 'name' | 'age'
+// 2. User['name' | 'age'] -> достаёт типы значений по этим ключам: string | number
+// Итог: в Test1 попадает объединённый тип string | number
+type Test1 = User[keyof User]
